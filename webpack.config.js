def6cb7881816ab67f3ff315e4d3efd77f83574d
Ayman Mahfouz <amahfouz@webalo.com>
2019-07-05 21:29:32 +0200

    Added config files.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: ['./app/app.js']
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.bpmn$/,
+        use: 'raw-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: '**/*.{html,css}', context: 'app/' }
+    ])
+  ],
+  mode: 'development',
+  devtool: 'source-map'
+};
